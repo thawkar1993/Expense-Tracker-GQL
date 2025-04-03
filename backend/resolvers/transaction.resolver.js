d8baa4e14de1cf2166bdd9482579d6a592945200
anantthawkar93 <anant1993thawkar@gmail.com>
2025-04-04 00:21:35 +0530

    transaction resolver added

--- backend/resolvers/transaction.resolver.js
+++ backend/resolvers/transaction.resolver.js
@@ -1,6 +1,75 @@
+import Transaction from '../models/transaction.model.js'
+
 const transactionResolver = {
-    Query: {},
-    Mutation: {}
+    Query: {
+        transactions: async (_, __, context) => {
+            try{
+                if(!context.getUser()) throw new Error("Unauthorized");
+                const userId = await context.getUser()._id;
+
+                const transactions = await Transaction.find({ userId: userId})
+                return transactions;
+
+            }catch(err){
+                console.error("Error in getting transactions: ", err);
+                throw new Error(err.message || "Internal server error");
+            }
+        },
+
+        transaction: async (_,{transactionId}, ) => {
+            try{
+                const transaction = await Transaction.findById(transactionId);
+                return transaction;
+
+            }catch(err){
+                console.error("Error in getting an transaction: ", err);
+                throw new Error(err.message || "Internal server error");
+            }
+        },
+
+        //TODO => Add CategoryStatistics Query
+    },
+    Mutation: {
+        createTransaction: async(_, {input}, context) => {
+            try{
+                const newTransaction = new Transaction({
+                    ...input,
+                    userId: context.getUser()._id,
+                })
+                await newTransaction.save();
+                return newTransaction;
+
+            }catch(err){
+                console.error("Error in creating transaction: ", err);
+                throw new Error(err.message || "Internal server error");
+            }
+        },
+
+        updateTransaction: async(_, {input}) => {
+            try{
+                const updatedTransaction = await Transaction.findByIdAndUpdate(input.transactionId, input, {new: true});
+                return updatedTransaction;
+
+            }catch(err){
+                console.error("Error in updating transaction: ", err);
+                throw new Error(err.message || "Internal server error");
+            }
+        },
+
+        deleteTransaction: async(_, { transactionId }) => {
+            try{
+                const deletedTransaction = await Transaction.findByIdAndDelete(transactionId);
+                return deletedTransaction;
+
+            }catch(err){
+                console.error("Error in deleting transaction: ", err);
+                throw new Error(err.message || "Internal server error");
+            }
+        },
+
+        //TODO => Add Transaction/User Relationship - later
+
+    }
 };
 
 export default transactionResolver;
--- backend/typeDefs/transaction.typeDef.js
+++ backend/typeDefs/transaction.typeDef.js
@@ -14,6 +14,7 @@ const transactionTypeDef = `#graphql
     type Query {
         transactions: [Transaction!]
         transaction(transactionId: ID!): Transaction
+        #TODO => Add CategoryStatistics Query
     }
 
     type Mutation {
